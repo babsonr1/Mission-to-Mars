a8cc00caa537144b8d26f0fa4da4115b06a15a7b
babsonr1 <riley.babson@du.edu>
2021-11-29 19:05:58 -0700

    Adjusted scraping.py to be composed of functions.

--- scraping.py
+++ scraping.py
@@ -1,50 +1,91 @@
-import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
+import pandas as pd
+import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
 
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+def scrapeAll():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
 
-html = browser.html
-newsSoup = soup(html, 'html.parser')
-slideElem = newsSoup.select_one('div.list_text')
+    newsTitle, newsParagraph = marsNews(browser)
 
-slideElem.find('div', class_='content_title')
+    data = {
+        "news_title": newsTitle,
+        "news_paragraph": newsParagraph,
+        "featured_image": featuredImage(browser),
+        "facts": marsFacts(),
+        "last_modified": dt.datetime.now()
+    }
 
-newsTitle = slideElem.find('div', class_='content_title').get_text()
-newsTitle
+    browser.quit()
+    return data
 
-newsp = slideElem.find('div', class_='article_teaser_body').get_text()
-newsp
 
-# ### Featured Images
+def marsNews(browser):
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    browser.visit(url)
 
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-fullImageElem = browser.find_by_tag('button')[1]
-fullImageElem.click()
+    html = browser.html
+    newsSoup = soup(html, 'html.parser')
 
-html = browser.html
-imgSoup = soup(html, 'html.parser')
+    try:
+        slideElem = newsSoup.select_one('div.list_text')
+        newsTitle = slideElem.find('div', class_='content_title').get_text()
+        newsp = slideElem.find('div', class_='article_teaser_body').get_text()
 
-imgUrlRel = imgSoup.find('img', class_='fancybox-image').get('src')
-imgUrlRel
+    except AttributeError:
+        return None, None
 
-imgUrl = f'https://spaceimages-mars.com/{imgUrlRel}'
-imgUrl
+    return newsTitle, newsp
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
 
-df.to_html()
+def featuredImage(browser):
+    # Visit URL
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)
 
-browser.quit()
+    # Find and click the full image button
+    fullImageElem = browser.find_by_tag('button')[1]
+    fullImageElem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    imgSoup = soup(html, 'html.parser')
+
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        imgUrlRel = imgSoup.find('img', class_='fancybox-image').get('src')
+
+    except AttributeError:
+        return None
+
+    # Use the base url to create an absolute url
+    imgUrl = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{imgUrlRel}'
+
+    return imgUrl
+
+def marsFacts():
+    # Add try/except for error handling
+    try:
+        # Use 'read_html' to scrape the facts table into a dataframe
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
+
+    except BaseException:
+        return None
+
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
+
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html(classes="table table-striped")
+
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrapeAll())
